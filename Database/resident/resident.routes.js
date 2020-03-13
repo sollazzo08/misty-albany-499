f7e83d8e5324d24547bd84bff58b892207ddec4e
Kyle Welch <30532096+kywelch17@users.noreply.github.com>
2020-03-13 17:07:24 -0400

    Added note

--- Database/resident/resident.routes.js
+++ Database/resident/resident.routes.js
@@ -5,10 +5,10 @@ module.exports = (app) => {
     app.post('/resident', resident.create);
 
     //Retrieving all residents from db
-    app.get('/resident', resident.findAll);
+    //app.get('/resident', resident.findAll);
 
     //Retrieving a single resident from db
-    app.get('/resident/:r_id', resident.findOne);
+    //app.get('/resident/:r_id', resident.findOne);
 
     //Update a resident with a r_id
     app.put('/resident/:r_id', resident.update);
@@ -16,3 +16,8 @@ module.exports = (app) => {
     //Deleting a resident
     app.delete('/resident/:r_id', resident.delete);
 }
+
+/**
+ * Note: We will decide later whether to list all the 
+ * rows in the homepage, or if we will just do a findOne().
+ */
