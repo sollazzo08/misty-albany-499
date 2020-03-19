d8d2c9a5ae2ce3d88ccbc4fce3920973dc4c9436
Michael Sollazzo <msollazzo08@gmail.com>
2020-03-19 02:28:29 -0400

    cleaned up frontend, got TestStart to work

--- staffdashRestAPI/app.js
+++ staffdashRestAPI/app.js
@@ -1,29 +1,151 @@
-
-var ipAddress = document.getElementById('ip');
-var connect = document.getElementById('connect');
-var ip;
-var socket;
-var client;
-
-connect.onclick = function() {
-  ip = validateIPAddress(ipAddress.value);
-  if (!ip) {
-    alert
-    return;
-  }
-  client = new LightClient(ip, 10000);
-  client.GetCommand("device", function(data) {
-    
-    console.log(data);
-  });
-};
-
-
-//Helper function 
+//Helper functions
 //Let misty pause to give her time to register and excute command
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+var ipAddress = document.getElementById('ip');
+var connect = document.getElementById('connect');
+var testStartSkill = document.getElementById('testStartSkill');
+var testStopSkill= document.getElementById('testStopSkill');
+var ip;
+//This Ip never gets recorded 
+
+
+const you = "Mike";
+let onList = false;
+
+
+connect.onclick = function() {
+	ip = validateIPAddress(ipAddress.value);
+	console.log(ip);
+	
+  if (!ip) {
+		console.log("IP address needed");
+    return;
+	}
+	client = new LightClient(ip, 10000);
+  client.GetCommand("device", function(data) {
+    console.log("Connected to Misty");
+		console.log(data);
+		
+  });
+};
+
+
+
+//Test start funciton 
+testStartSkill.onclick = function() {
+	socket = new LightSocket(ip, startTest);
+	ip = validateIPAddress(ipAddress.value);	
+  if (!ip) {
+    console.log("You must connect to a robot first.");
+    return;
+	}
+	//console.log(ip);
+	startTest();
+	socket.Connect();
+};
+
+testStopSkill.onclick = function() {
+  stopTest();
+};
+
+
+ async function startTest() {
+
+		socket.Unsubscribe('FaceRecognition');
+		await sleep(3000);
+	
+		axios.get("http://" + ip + "/api/faces")
+			.then(function (res) {
+				let faceArr = res.data.result;
+				console.log("Learned faces: ", faceArr);
+	
+					for(let i = 0; i < faceArr.length; i++){
+						if(faceArr[i] === you){
+							onList = true;
+						}
+					}
+	
+					socket.Subscribe("FaceRecognition", "FaceRecognition", 200, null, null, null, null, _FaceRecognition);   
+					console.log("subscribing to FaceRecognition");
+					
+					if(onList) {
+						console.log('You were found on the list!');
+							startFaceRecognition();
+					} else {
+						console.log('You were not on the list');
+							startFaceTraining();
+					}
+	
+			});
+	}
+
+
+function stopTest() {
+	client.PostCommand("faces/detection/stop");
+	console.log("Stopping Test");
+	
+  socket.close();
+  messageCount = 0;
+}
+
+
+function startFaceRecognition() {
+  console.log("starting Test..");   
+    axios.post("http://" + ip + "/api/faces/recognition/start");
+};
+
+async function startFaceTraining() {
+  console.log("starting face training");
+    axios.post("http://" + ip + "/api/faces/training/start", { FaceId: you });
+    await sleep(20000);
+  
+    console.log("face training complete");
+    axios.post("http://" + ip + "/api/faces/recognition/start");
+};
+
+function _FaceRecognition(data, lightClient) {
+  try {
+      if (data.message.personName !== "unknown person" && data.message.personName !== null && data.message.personName !== undefined) {
+          // If the face is recognized, print a 
+          // message to greet the person by name.
+      
+          console.log(`A face was recognized. Hello there ${data.message.personName}!`);
+					//POST http://192.168.1.151/api/tts/speak
+					
+				Promise.race([
+					fetch('http://'+ ip + '/api/tts/speak?text=Hello Mike&flush=false', {
+						method: 'POST',
+						body: '{ "text":"Hello Mike","flush":false,"utteranceId":null }'
+					}),
+					new Promise((_, reject) => setTimeout(() => reject(new Error('timeout')), 10000))
+				])
+				.then(response => response.json())
+				.then(jsonData => console.log(jsonData))
+         
+
+
+          // Unsubscribe from the FaceRecognition WebSocket.
+          socket.Unsubscribe("FaceRecognition");
+          // Use Axios to issue a POST command to the 
+          // endpoint for the StopFaceRecognition command.
+          axios.post("http://" + ip + "/api/faces/recognition/stop");
+      }
+  }
+  catch (e) {
+      console.log("Error: " + e);
+  }
+}
+
+
+
+
+
+
+
 
 function validateIPAddress(ip) {
 	var ipNumbers = ip.split(".");
